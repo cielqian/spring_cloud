d3f7a777e8e06d79a94a0ac0b76989b84341a4d4
CielQian <qianhong91@outlook.com>
2018-05-30 16:58:58 +0800

    spring cloud hystrix dashboard

---
+++ spring_cloud_consumer_hystrix_dashboard/src/main/java/com/ciel/remote/HelloRemote.java
@@ -0,0 +1,12 @@
+package com.ciel.remote;
+
+import com.ciel.hystrix.HelloRemoteHystrix;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
+public interface HelloRemote {
+    @RequestMapping(value = "/hello/msg")
+    public String hello(@RequestParam(value = "name") String name);
+}
