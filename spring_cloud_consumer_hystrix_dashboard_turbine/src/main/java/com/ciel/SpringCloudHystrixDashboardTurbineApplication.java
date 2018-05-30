22cdf0950243417f9a9d0256a50d69df7d344225
CielQian <qianhong91@outlook.com>
2018-05-30 17:28:28 +0800

    spring cloud hystrix turbine

---
+++ spring_cloud_consumer_hystrix_dashboard_turbine/src/main/java/com/ciel/SpringCloudHystrixDashboardTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.ciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+public class SpringCloudHystrixDashboardTurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixDashboardTurbineApplication.class, args);
+    }
+}
