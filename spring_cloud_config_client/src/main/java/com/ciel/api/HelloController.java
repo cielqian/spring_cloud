80457b0f33f0c82f3a48fda338cbf3a9874f2b5e
CielQian <qianhong91@outlook.com>
2018-06-01 16:51:09 +0800

    spring cloud config eureka

--- spring_cloud_config_client/src/main/java/com/ciel/api/HelloController.java
+++ spring_cloud_config_client/src/main/java/com/ciel/api/HelloController.java
@@ -1,12 +1,14 @@
 package com.ciel.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${neo.hello}")
     private String hello;
